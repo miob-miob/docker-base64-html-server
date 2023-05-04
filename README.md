a75e720452737ee3df68dd4721b358bef828aa0f
michal.oboril <michal.oboril@bloomreach.com>
2023-05-04 17:18:03 +0200

    nginx as non root user

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 ## nginx server which will serve HTML file provided as base 64 encoded environment
 
+#### default port is 8080 !!!
+because non privileged port
 
 ---
 **usage:**
 
 ```shell
-docker run -it --rm -p 8888:80 -e BASE64_HTML=$(echo "<div>Hi Hi</div>" | base64) miob01/base64-html-server
+docker run -it --rm -p 8888:8080 -e BASE64_HTML=$(echo "<div>Hi Hi</div>" | base64) miob01/base64-html-server
 ```
+---
 
 For more details:
 ```shell
-docker run -it --rm -p 8888:80  miob01/base64-html-server
+docker run -it --rm -p 8888:8080  miob01/base64-html-server
 ```
 navigate browser on: [localhost:8888](http://localhost:8888/)
 
--- build_and_push.sh
+++ build_and_push.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-set -ue
+set -u
 
-image_tag=miob01/base64-html-server:latest
+new_version=0.1.0
+
+#colors
+DICKEN_GREEN='\033[1;32m'
+GREEN='\033[0;32m'
+DICKEN_RED='\033[1;31m'
+RED='\033[0;31m'
+NO_COLOUR='\033[0m'
+
+
+repo=miob01/base64-html-server
+latest_full=${repo}:latest
+new_full=${repo}:${new_version}
+
 #docker login
 
 # default buildx instance is not capable of multiplatform builds, we can create container to work for us as builder
 #docker buildx create --driver docker-container --platform linux/arm64,linux/arm/v8,linux/amd64 --name miob_builder --use --bootstrap
-
-
-docker buildx build -f Dockerfile --platform linux/amd64,linux/arm64/v8 -t $image_tag --push .
+docker buildx use miob_builder
+docker pull ${new_full}
+if [ $? -eq 0 ]; then
+  echo -e "\n${RED}Tag: ${DICKEN_RED} ${new_full} ${RED} already exists!${NO_COLOUR}"
+  exit 1
+fi
+set -e
+docker buildx build -f Dockerfile --platform linux/amd64,linux/arm64/v8 -t $latest_full -t $new_full --push .
+echo -e "\n${GREEN} Done ;) ${NO_COLOUR}"
 
